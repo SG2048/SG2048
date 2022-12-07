bb41f5b025798a319d98b916a734a2334bba84ab
SG2048 <101252855+SG2048@users.noreply.github.com>
2022-12-07 13:49:44 +0000

    Removed cheat function

--- tabscript.js
+++ tabscript.js
@@ -25,11 +25,6 @@ var numberpersecond = () => (autoclickerlevel > 0) ? (constants.automult ** (aut
 document.addEventListener("keydown", cheatfunction)
 
 
-function cheatfunction(e) {
-    console.log("you pressed a key, which is " + (10**((Number(e.key)**2)*2)))
-    currency =  (10**((Number(e.key)**2)*2))
-}
-
 currency = currency + ((numberpersecond()) * ((new Date().valueOf()) - Number((localStorage.getItem("storedtime"))))) / 3000
 display()
 
